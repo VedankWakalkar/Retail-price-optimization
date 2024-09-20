0bd3336e2542937fe830e474aa26210a469e28e6
VedankWakalkar <131476793+VedankWakalkar@users.noreply.github.com>
2024-09-20 17:48:26 +0530

    Added DateFeatureEngg

--- steps/process_data.py
+++ steps/process_data.py
@@ -2,14 +2,27 @@ import pandas as pd
 from zenml import step
 from zenml.logger import get_logger
 from steps.src.data_processor import CategoricalEncoder
+from steps.src.feature_eng import DateFeatureEngineer
 
 logger= get_logger(__name__)
 
-@step()
+@step
 def catagorical_encoding(df:pd.DataFrame)->pd.DataFrame:
     try:
         encoder= CategoricalEncoder(method="oneshot")
         df=encoder.fit_transform(df,columns=["product_id","product_category_name"])
         return df
+    except Exception as e:
+        raise e
+    
+@step
+def feature_engg(df:pd.DataFrame)->pd.DataFrame:
+    try:
+        date_enginer=DateFeatureEngineer(date_format="%d-%m-%Y")
+        df_transformed= date_enginer.fit_transform(df,columns=["month_year"])
+        logger.info("Successfully engineered features")
+
+        df_transformed.drop(["id","month_year"],axis=1,inplace=True)
+        return df_transformed
     except Exception as e:
         raise e
--- steps/src/feature_eng.py
+++ steps/src/feature_eng.py
@@ -5,5 +5,20 @@ import pandas as pd
 class FeatureEngineer(ABC):
     @abstractmethod
     def fit_transform(self, df:pd.DataFrame,columns:List[str])->pd.DataFrame:
+        pass
          
-class DataFeatureEngineer()
+class DateFeatureEngineer(FeatureEngineer):
+    def __init__(self,date_format:str="%m-$d-%Y"):
+        self.date_format= date_format
+
+    def fit_transform(self, df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
+        for column in columns:
+            df=self.split_date(df,column)
+        return df
+    
+    def split_date(self, df:pd.DataFrame, column:str)->pd.DataFrame:
+         df[column]=pd.to_datetime(df[column],format=self.date_format)
+         df[f'{column}_year']=df[column].dt.year
+         df[f'{column}_month']=df[column].dt.month
+         return df
+    
